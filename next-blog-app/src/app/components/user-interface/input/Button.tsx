e62dfa15a28b3e5d5b5a4505473273c4f9f2ef90
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-05-03 00:45:21 +0700

    Create Button.tsx

---
+++ next-blog-app/src/app/components/user-interface/input/Button.tsx
@@ -0,0 +1,19 @@
+export default function Button({
+    label,
+    onClick,
+    type = "submit",
+}: {
+    label: string;
+    onClick: () => void;
+    type?: "submit" | "reset";
+}) {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            className={`${type === "submit" ? 'bg-secondary' : "bg-warning"} text-primary-variant hover:brightness-90 rounded-md p-2`}
+        >
+            {label}
+        </button>
+    );
+}
