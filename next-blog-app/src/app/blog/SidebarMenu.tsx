822863930b77213e023ad050ac86426b385ba3e4
meow <69355934+MeowKyWay@users.noreply.github.com>
2025-05-05 02:57:34 +0700

    Polishing

--- next-blog-app/src/app/blog/SidebarMenu.tsx
+++ next-blog-app/src/app/blog/SidebarMenu.tsx
@@ -3,14 +3,15 @@
 
 import { ReactNode, useState } from "react";
 import { View } from "../page";
-import { HomeIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
+import { ArrowRightIcon, HomeIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter } from "next/navigation";
 
 type Props = {
     isOpen: boolean;
+    onClose: () => void;
 };
 
-export default function SidebarMenu({ isOpen }: Props) {
+export default function SidebarMenu({ isOpen, onClose }: Props) {
 
     const router = useRouter();
     const path = usePathname();
@@ -18,19 +19,27 @@ export default function SidebarMenu({ isOpen }: Props) {
     const iconClassName = "w-6 h-6 stroke-2";
 
     return (
-        <div className={`w-[280px] md:flex-1 bg-primary pt-8 h-full absolute z-10
+        <>
+            <div className={`w-[280px] md:flex-1 bg-primary pt-8 h-full absolute z-20
             transition-[right] duration-300 ease-in-out 
             top-0 ${isOpen ? 'right-0' : '-right-70'} h-full 
             md:static md:h-full md:!bg-(--background)`}>
-            <nav className="flex flex-col gap-2">
-                <SidebarMenuItem icon={(<HomeIcon className={iconClassName} />)}
-                    label="Home" isSelected={path === '/blog'}
-                    onClick={() => router.push('/blog/')} />
-                <SidebarMenuItem icon={(<PencilSquareIcon className={iconClassName} />)}
-                    label="My Blog" isSelected={path === '/blog/my-blog'}
-                    onClick={() => router.push('/blog/my-blog')} />
-            </nav>
-        </div>
+                <nav className="flex flex-col gap-2">
+                    <div className="flex items-center justify-center p-2 rounded-full md:hidden
+                    hover:bg-black/5 hover:bg-black/10 w-10 h-10 ml-4"
+                        onClick={onClose}>
+                        <ArrowRightIcon className="w-6 h-6 stroke-2 text-primary-variant md:hidden" />
+                    </div>
+                    <SidebarMenuItem icon={(<HomeIcon className={iconClassName} />)}
+                        label="Home" isSelected={path === '/blog'}
+                        onClick={() => router.push('/blog/')} />
+                    <SidebarMenuItem icon={(<PencilSquareIcon className={iconClassName} />)}
+                        label="My Blog" isSelected={path === '/blog/my-blog'}
+                        onClick={() => router.push('/blog/my-blog')} />
+                </nav>
+            </div>
+            {isOpen && <div className={`md:hidden fixed inset-0 bg-black/50 z-10`} />}
+        </>
     );
 }
 
--- next-blog-app/src/app/blog/layout.tsx
+++ next-blog-app/src/app/blog/layout.tsx
@@ -4,6 +4,10 @@ import { Bars3Icon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 import SidebarMenu from "./SidebarMenu";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useAuth } from "../providers/AuthProvider";
+import { UserCircle } from "../components/user-interface/UserCircle";
+import Button from "../components/user-interface/input/Button";
+import { useRouter } from "next/navigation";
 
 export default function HomeLayout({
     children,
@@ -14,12 +18,21 @@ export default function HomeLayout({
 
     const queryClient = new QueryClient();
 
+    const { username, logout } = useAuth();
+
+    const router = useRouter();
+
     return (
         <QueryClientProvider client={queryClient}>
-            <div className="flex flex-col bg-background h-screen w-screen overflow-hidden">
-                {/* static */}
+            <div className="flex flex-col bg-background h-screen w-screen overflow-hidden relative">
                 <div className="flex flex-row bg-primary w-screen h-[72px] md:h-[60px] items-center px-4 md:px-8 justify-between">
                     <div className="castoro text-primary-variant italic text-[24px] font-[400]">a Board</div>
+                    {username && <div className="hidden md:flex flex-row gap-5 w-30 h-10 items-center text-primary-variant mr-6">
+                        {username}
+                        <UserCircle />
+                    </div>}
+                    {!username && <Button className="h-10 px-6" label="Sign in" type="submit" onClick={() => router.push('/sign-in')}>
+                    </Button>}
                     <button
                         onClick={() => setIsOpen((prev) => !prev)}
                         className="md:hidden rounded-full p-2 bg-primary
@@ -27,8 +40,8 @@ export default function HomeLayout({
                         <Bars3Icon className="h-6 text-primary-variant" />
                     </button>
                 </div>
-                <div className="flex flex-row w-screen h-[calc(100vh-72px)] md:h-[calc(100vh-60px)] relative">
-                    <SidebarMenu isOpen={isOpen} />
+                <div className="flex flex-row w-screen h-[calc(100vh-72px)] md:h-[calc(100vh-60px)]">
+                    <SidebarMenu isOpen={isOpen} onClose={() => setIsOpen(false)} />
                     <div className="flex-4">
                         <main className="h-full bg-background">
                             {children}
